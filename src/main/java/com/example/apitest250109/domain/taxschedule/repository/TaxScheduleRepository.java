cd12cfd98ce375bcf9f009174ea563c4d2b0efd7
SeungHo <gemdoq@gmail.com>
2025-01-13 11:47:34 +0900

    feat(taxSchedule): create TaxScheduleRepository

--- src/main/java/com/example/apitest250109/domain/taxschedule/repository/TaxScheduleRepository.java
+++ src/main/java/com/example/apitest250109/domain/taxschedule/repository/TaxScheduleRepository.java
@@ -1,4 +1,7 @@
 package com.example.apitest250109.domain.taxschedule.repository;
 
-public interface TaxScheduleRepository {
+import com.example.apitest250109.domain.taxschedule.model.entity.TaxScheduleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaxScheduleRepository extends JpaRepository<TaxScheduleEntity, Long> {
 }
