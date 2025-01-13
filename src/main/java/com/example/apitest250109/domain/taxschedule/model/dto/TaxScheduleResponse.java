32532e227682e293030a83c2b831100585722792
SeungHo <gemdoq@gmail.com>
2025-01-13 11:46:04 +0900

    feat(dto): create dto for api response

--- src/main/java/com/example/apitest250109/domain/taxschedule/model/dto/TaxScheduleResponse.java
+++ src/main/java/com/example/apitest250109/domain/taxschedule/model/dto/TaxScheduleResponse.java
@@ -1,4 +1,28 @@
 package com.example.apitest250109.domain.taxschedule.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class TaxScheduleResponse {
+
+	@JsonProperty("currentCount")
+	private int currentCount;
+
+	@JsonProperty("data")
+	private List<DataItem> data;
+
+	@Data
+	public static class DataItem {
+		@JsonProperty("세무내용")
+		private String taxContent;
+
+		@JsonProperty("세무일정")
+		private String scheduleDate;
+
+		@JsonProperty("비고")
+		private String remark;
+	}
 }
