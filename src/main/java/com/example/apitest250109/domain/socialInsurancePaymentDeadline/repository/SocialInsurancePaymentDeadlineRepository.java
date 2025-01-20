e889256b64389340fd51f873cc15dae8e9250b21
SeungHo <gemdoq@gmail.com>
2025-01-20 23:37:41 +0900

    feat(socialInsurancePaymentDeadline): create a repository to persist response data for socialInsurancePaymentDeadline

---
+++ src/main/java/com/example/apitest250109/domain/socialInsurancePaymentDeadline/repository/SocialInsurancePaymentDeadlineRepository.java
@@ -0,0 +1,7 @@
+package com.example.apitest250109.domain.socialInsurancePaymentDeadline.repository;
+
+import com.example.apitest250109.domain.socialInsurancePaymentDeadline.model.entity.SocialInsurancePaymentDeadlineEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SocialInsurancePaymentDeadlineRepository extends JpaRepository<SocialInsurancePaymentDeadlineEntity, Long> {
+}
