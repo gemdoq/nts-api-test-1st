b1d5bd96d90bbf2f6d0fcdeb7d6a93dd16057916
SeungHo <gemdoq@gmail.com>
2025-01-20 23:47:18 +0900

    feat(taxSchedule): update test code for feignclient

--- src/test/java/com/example/apitest250109/global/api/guksechung/TaxScheduleFeignClientTest.java
+++ src/test/java/com/example/apitest250109/global/api/guksechung/TaxScheduleFeignClientTest.java
@@ -3,18 +3,14 @@ package com.example.apitest250109.global.api.guksechung;
 import com.example.apitest250109.domain.taxschedule.model.dto.TaxScheduleResponse;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Value;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 public class TaxScheduleFeignClientTest {
 
-	@Value("${TAX_SCHEDULE_API_RETURN_TYPE}")
-	private String defaultReturnType;
-
-	@Value("${TAX_SCHEDULE_API_SERVICE_KEY}")
-	private String defaultServiceKey;
+	private String defaultReturnType = "json";
+	private String defaultServiceKey = "serviceKey";
 
 	private final TaxScheduleFeignClient feignClient = Mockito.mock(TaxScheduleFeignClient.class);
 
@@ -27,9 +23,7 @@ public class TaxScheduleFeignClientTest {
 		when(feignClient.getTaxSchedules(1, 300, defaultReturnType, defaultServiceKey))
 				.thenReturn(expectedResponse);
 
-
 		TaxScheduleResponse actualResponse = feignClient.getTaxSchedules(1, 300, defaultReturnType, defaultServiceKey);
-
 
 		assertEquals(expectedResponse.getCurrentCount(), actualResponse.getCurrentCount());
 	}
